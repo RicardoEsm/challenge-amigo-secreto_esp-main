338e8056880779fcea8f9569f36f1834d703ae78
Ricardo.espinoza <ricardo.espinoza31@gmail.com>
2025-03-08 19:42:48 -0300

    Version 1 con logica basica, añadiendo mensajes de error y validacion de codigo vacio (Aun queda pendiente manejo de otros errores)

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let listaNombres=[];
+
+function agregarAmigo(){
+    let nombre=document.getElementById("amigo").value;
+    console.log(nombre);
+    
+    if(nombre===""){
+        mensaje.innerText ="El nombre no puede estar vacio";
+        console.log("Vacio")
+        return;
+    }
+    
+    listaNombres.push(nombre);
+    document.getElementById("botonAñadir").addEventListener("click",verLista);
+}
+
+
+
+
+function verLista(){
+    
+    let nombres=document.getElementById("listaAmigos").innerText =listaNombres;
+    console.log(nombres);
+}
+
+
+function sortearAmigo(){
+
+}

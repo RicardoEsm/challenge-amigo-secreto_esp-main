763065b4a51de965836da8f523a4ff79e34d0da0
Ricardo.espinoza <ricardo.espinoza31@gmail.com>
2025-03-08 20:29:16 -0300

    Se definieron mejor las estructuras y se actualizo el boton de sorteo. Queda pendiente resetar el juego una vez sorteado

--- app.js
+++ app.js
@@ -4,29 +4,47 @@
 let listaNombres=[];
 
 function agregarAmigo(){
-    let nombre=document.getElementById("amigo").value;
+    let nombre=document.getElementById("amigo").value.trim();
     console.log(nombre);
-    
-    if(nombre===""){
-        mensaje.innerText ="El nombre no puede estar vacio";
-        console.log("Vacio")
-        return;
+    if(!validarLista(nombre)){
+        return;//
     }
-    
     listaNombres.push(nombre);
     document.getElementById("botonAñadir").addEventListener("click",verLista);
 }
 
-
-
+function validarLista(nombre){
+    
+    let respuesta = document.getElementById("mensaje");
+    if(nombre===""){
+        respuesta.innerText ="El nombre no puede estar vacio";
+        console.log("Vacio")
+        document.getElementById("amigo").value = ""
+        return;
+    }if(listaNombres.includes(nombre)){
+        respuesta.innerText = "Nombre ya incluido";
+        console.log("Duplicado");
+        document.getElementById("amigo").value = ""
+        return;
+    }
+    
+    respuesta.innerText = "";
+    return true;
+}
 
 function verLista(){
     
-    let nombres=document.getElementById("listaAmigos").innerText =listaNombres;
+    let nombres=document.getElementById("listaAmigos").innerText =`Los participantes son: ${listaNombres}`;
     console.log(nombres);
 }
 
 
 function sortearAmigo(){
-
+    let sorteado= parseInt(Math.floor(Math.random()*listaNombres.length));  // Genero numero entero aleatorio entre 0 y 1 con maximo igual al largo del array.
+    console.log(sorteado);
+    console.log(listaNombres[sorteado]);
+    document.getElementById("resultado").innerHTML = ` El ganador del sorteo es: ${listaNombres[sorteado]} ¡Felicidades!`;
+    document.getElementById("resultado").style.color = "green";
 }
+document.getElementById("botonSorteo").addEventListener("click",sortearAmigo);
+
